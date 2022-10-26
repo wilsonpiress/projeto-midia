200334501af963150ac2475ac292a58a11087a96
wilsonpiress <wilsonpires.js@gmail.com>
2022-10-25 22:04:03 -0300

    Foram implementadas as principais funcionalidades

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio)
+    
+    tecla.onclick = function() {
+        tocaSom  (idAudio);
+    }
+        
+    contador = contador + 1;
+
+    //console.log(contador);
+}
+
