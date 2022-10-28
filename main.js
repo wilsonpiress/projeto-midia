d5a4400ef069fb940f07e959c50800a85ef7306e
wilsonpiress <wilsonpires.js@gmail.com>
2022-10-28 13:21:22 -0300

    Otimizei o JavaScript para ter um melhor código.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,5 +23,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom  (idAudio);
     }
 
+    tecla.onkeydown = function (evento) {
+        
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+        
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
